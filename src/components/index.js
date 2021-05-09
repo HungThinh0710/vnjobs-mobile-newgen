c2ea86feadecf14bea1761fdc6220dd4a00a32b9
SinJunior <khtuankendy@gmail.com>
2021-05-09 16:56:56 +0700

    Menu screen component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 import Search from './Search';
+import TitleMore from './TitleMore';
+import BlurBg from './BlurBg';
+import PopularJobItem from './PopularJobItem';
+import RecentJobItem from './RecentJobItem';
 
-export {Search};
+export {Search, TitleMore, BlurBg, PopularJobItem, RecentJobItem};
--- src/screens/Menu/index.js
+++ src/screens/Menu/index.js
@@ -1,16 +1,110 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+    FlatList,
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    View,
+} from 'react-native';
 import {Icon} from 'react-native-elements';
-import {Search} from '../../components';
+import {
+    BlurBg,
+    PopularJobItem,
+    RecentJobItem,
+    Search,
+    TitleMore,
+} from '../../components';
 
 const Menu = () => {
+    const initData = [
+        {
+            id: 'asdf3-243153-faweqr-24332',
+            name: 'Apple',
+            address: 'LA',
+            job: 'UX/UI Designer',
+            salary: '80-90K/year',
+            type: ['Fulltime', 'Remote'],
+            lefttime: '2 days left',
+        },
+        {
+            id: 'sdf-243153-faweqr-24332',
+            name: 'Apple',
+            address: 'LA',
+            job: 'UX/UI Designer',
+            salary: '80-90K/year',
+            type: ['Fulltime', 'Remote'],
+            lefttime: '2 days left',
+        },
+        {
+            id: 'sdf-543153-faweqr-24332',
+            name: 'Apple',
+            address: 'LA',
+            job: 'UX/UI Designer',
+            salary: '80-90K/year',
+            type: ['Fulltime', 'Remote'],
+            lefttime: '2 days left',
+        },
+        {
+            id: 'sdf-543153-dfasf-24332',
+            name: 'Google',
+            address: 'Singapore',
+            job: 'UX/UI Designer',
+            salary: '80-90K/year',
+            type: ['Fulltime', 'Remote'],
+            lefttime: '2 days left',
+        },
+    ];
+
     return (
-        <View>
-            <Search />
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView nestedScrollEnabled={true} showsVerticalScrollIndicator={false}>
+                <BlurBg />
+                <View style={styles.wrapperContent}>
+                    <Search />
+                    <TitleMore title="Popular Job" more="See all" />
+                    <FlatList
+                        style={styles.flatlistViewHorizontal}
+                        horizontal={true}
+                        data={initData}
+                        renderItem={items => {
+                            return <PopularJobItem item={items.item} />;
+                        }}
+                        keyExtractor={item => item.id}
+                    />
+                    <TitleMore title="Recent Job" more="See all" />
+                    <FlatList
+                        style={styles.flatlistView}
+                        data={initData}
+                        renderItem={items => {
+                            return <RecentJobItem item={items.item} />;
+                        }}
+                        keyExtractor={item => item.id}
+                    />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 export default Menu;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    wrapperContent: {
+        flex: 1,
+        zIndex: 2,
+    },
+    flatlistViewHorizontal: {
+        maxHeight: 175,
+        height: 175,
+        marginBottom: 10,
+    },
+    flatlistView: {
+        flex: 1,
+        maxHeight: 320,
+        height: 320,
+    },
+});
